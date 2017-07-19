1bcd952eabd90d194a0c51f4d0dd0bf105bb4e1f
Joey <caoyi163@gmail.com>
2017-07-18 22:25:20 -0400

    changes on feature1

--- ex3.py
+++ ex3.py
@@ -1,6 +1,8 @@
 after git stash the working directory, changes made here to
 move the HEAD forward. to test conflict.
 
+hello world.
+
 print "I will now count my chickens:"
 
 print "Hens", 25 + 30 / 6
